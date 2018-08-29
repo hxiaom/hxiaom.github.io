1b26a437ff4832e12e7140618962ce6b84dfa5f2
黄晓明 <447230830@qq.com>
2018-08-29 20:00:01 +0800

    delete static

--- static/js/default.js
+++
@@ -1,9 +0,0 @@
-$(document).scroll(function(){
-    if($(window).scrollTop()>=$('#wrap').position().top){
-    	if (!$('body').hasClass("read_mode"))
-        	$('body').addClass("read_mode");
-    } else {
-    	if ($('body').hasClass("read_mode"))
-        	$('body').removeClass("read_mode");
-    }
-})
